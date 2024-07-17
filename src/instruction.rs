a1df71278130e79048e86d25081acea81b0a23f1
Hank <hank.butler@protonmail.com>
2024-07-17 14:12:05 -0700

    Update instruction.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -27,3 +27,41 @@ pub enum VaultInstruction {
     }
 
 }
+
+pub enum StrategyInstruction {
+
+    Deposit {
+        amount: u64
+    },
+
+    Withdraw {
+        amount: u64
+    },
+}
+
+impl StrategyInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 | 1 => {
+                let amount = rest
+                    .get(..8)
+                    .and_then(|slice| slice.try_into().ok())
+                    .map(u64::from_le_bytes)
+                    .ok_or(InvalidInstruction)?;
+                match tag {
+                    1 => Self::Deposit { amount },
+                    2 => Self::Withdraw { amount },
+                    _ => return Err(VaultError::InvalidInstruction.into()),
+                }
+            }
+            _ => return Err(ValutError::InvalidInstruction.into()),       
+        })
+    }
+    
+    fn pack(&self) -> Vec<u8> {
+        
+    }
+}
+
