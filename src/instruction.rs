18c6139a73f6d777a9fc88012f1bef83081a1411
Hank <hank.butler@protonmail.com>
2024-07-17 13:56:48 -0700

    logic for initializing vault

--- src/instruction.rs
+++ src/instruction.rs
@@ -11,5 +11,19 @@ use std::convert::TryInto;
 use std::mem::size_of;
 
 pub enum VaultInstruction {
-    
+
+    InitializeVault{
+        strategy_program_deposit_instruction_id: u8,
+        strategy_program_withdraw_instruction_id: u8,
+        hold: bool,
+    },
+
+    Deposit {
+        amount: u64
+    },
+
+    Withdraw {
+        amount: u64,
+    }
+
 }
