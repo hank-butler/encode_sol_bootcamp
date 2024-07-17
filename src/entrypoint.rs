2114fd4f194788562539104525a59483122fa0d4
Hank <hank.butler@protonmail.com>
2024-07-16 22:51:48 -0700

    finished logic of entrypoint

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -4,4 +4,12 @@ use solana_program::{
 
 use crate::processor::Processor;
 
-entry
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Processor::process(program_id, accounts, instruction_data)
+}
