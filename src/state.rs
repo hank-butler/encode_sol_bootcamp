0967d8909501990363bb5701dcfc4a7dab3529de
Hank <hank.butler@protonmail.com>
2024-07-17 23:03:53 -0700

    impl pack for vault complete

--- src/state.rs
+++ src/state.rs
@@ -65,4 +65,42 @@ impl Pack for Vault {
         })
     }
 
-    fn pack_into_
+    fn pack_into_slice(&self, dst: &mut [u8]){
+        let dst = array_mut_ref![dst, 0, Vault::LEN];
+
+        let (
+            is_initialized_dst,
+            hold_dst,
+            llx_token_mint_id_dst,
+            lx_token_account_dst,
+            x_token_account_dst,
+            strategy_program_id_dst,
+            strategy_program_deposit_instruction_id_dst,
+            strategy_program_withdraw_instruction_id_dst,
+            strategy_data_account_dst,
+        ) = mut_array_refs![dst, 1, 1, 32, 32, 36, 32, 1, 1, 36];
+
+        let Vault {
+            is_initialized,
+            hold,
+            llx_token_mint_id,
+            lx_token_account,
+            x_token_account,
+            strategy_program_id,
+            strategy_program_deposit_instruction_id,
+            strategy_program_withdraw_instruction_id,
+            strategy_data_account
+        } = self;
+
+        is_initialized_dst[0] = *is_initialized as u8;
+        hold_dst[0] = *hold as u8;
+        llx_token_mint_id_dst.copy_from_slice(llx_token_mint_id.as_ref());
+        lx_token_account_dst.copy_from_slice(llx_token_mint_id.as_ref());
+        pack_coption_key(x_token_account, x_token_account_dst);
+        strategy_program_id_dst.copy_from_slice(strategy_program_id.as_ref());
+        strategy_program_deposit_instruction_id_dst[0] = *strategy_program_deposit_instruction_id;
+        strategy_program_withdraw_instruction_id_dst[0] = *strategy_program_withdraw_instruction_id;
+        pack_coption_key(strategy_data_account, strategy_data_account_dst);
+    }
+
+}
